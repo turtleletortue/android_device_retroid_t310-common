5cccc64e01350063293b0c282e2d15d75f8a2c96
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2023-03-30 13:36:29 -0400

    t310: Set AB_OTA_POSTINSTALL_CONFIG * Fixes addon.d not persisting through OTAs

--- t310.mk
+++ t310.mk
@@ -108,6 +108,16 @@ PRODUCT_PACKAGES += \
     update_engine_sideload \
     update_verifier
 
+# A/B Configs
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
 # Pservice **NEEDS** these
 PRODUCT_PACKAGES += \
     libstdc++.vendor 
