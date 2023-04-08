f99fa855397066a56392489e3dbfc908706b999d
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2023-04-08 18:27:01 -0400

    hidl: Build AOSP bluetooth audio hidl

--- hidl.mk
+++ hidl.mk
@@ -2,7 +2,8 @@
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.service
+    android.hardware.audio.service \
+    android.hardware.bluetooth.audio@2.0-impl
 
 # Boot
 PRODUCT_PACKAGES += \
